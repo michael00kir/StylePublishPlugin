235ea219065ea9f426c900555632df124e4415c7
michael00kir <michaelkir@michaelkir.com>
2024-10-13 20:50:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,18 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "StylePublishPlugin",
+    platforms: [.macOS(.v12)],
     products: [
         .library(
             name: "StylePublishPlugin",
             targets: ["StylePublishPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/Publish", from: "0.5.0")
+        .package(url: "https://github.com/JohnSundell/Publish", from: "0.8.0")
     ],
     targets: [
         .target(
